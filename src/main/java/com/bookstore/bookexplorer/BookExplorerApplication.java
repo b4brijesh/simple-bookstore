6921116e3976a2ba09458240d3c044f161282c8d
brijs <bsahoonap@gmail.com>
2020-05-16 12:48:28 +0000

    Initialize bookstore project

---
+++ src/main/java/com/bookstore/bookexplorer/BookExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExplorerApplication.class, args);
+	}
+
+}
